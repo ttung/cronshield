d7f5903bab9c474b3d3ec38d5c8e9cc00f082727
Tony Tung <tonytung@merly.org>
2019-11-05 13:00:22 -0800

    basic workflow

--- cronshield/cli.py
+++ cronshield/cli.py
@@ -1,17 +1,36 @@
 # -*- coding: utf-8 -*-
 
 """Console script for cronshield."""
+import subprocess
 import sys
+
 import click
 
 
 @click.command()
-def main(args=None):
+@click.argument("command", nargs=-1, required=True, type=str)
+def main(command):
     """Console script for cronshield."""
-    click.echo("Replace this message by putting your code into "
-               "cronshield.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+    process = subprocess.run(args=command, capture_output=True)
+
+    if process.returncode == 0:
+        sys.exit(0)
+
+    command_text = " ".join(command)
+
+    print(f"cronshield detected failure or error output for the command: {command_text}")
+    print(f"")
+    print(f"RESULT CODE: {process.returncode}")
+    if len(process.stdout) > 0:
+        print(f"")
+        print(f"OUTPUT:")
+        print(f"{process.stdout.decode('latin-1')}")
+    if len(process.stderr) > 0:
+        print(f"")
+        print(f"ERROR:")
+        print(f"{process.stderr.decode('latin-1')}")
+
+    sys.exit(process.returncode)
 
 
 if __name__ == "__main__":
--- tests/test_cronshield.py
+++ tests/test_cronshield.py
@@ -3,36 +3,41 @@
 
 """Tests for `cronshield` package."""
 
-import pytest
+import re
 
 from click.testing import CliRunner
 
-from cronshield import cronshield
 from cronshield import cli
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
+def test_no_args():
+    """Test the CLI with no args."""
     runner = CliRunner()
     result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'cronshield.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    assert result.exit_code == 2
+    assert "Missing argument" in result.output
+
+
+def test_help():
+    """Test the CLI's --help option."""
+    runner = CliRunner()
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert re.search(r"--help\s+Show this message and exit.", help_result.output) is not None
+
+
+def test_success():
+    """Test that the successful execution suppresses output."""
+    runner = CliRunner()
+    help_result = runner.invoke(cli.main, ["--", "python", "-c", "import sys; print('hello'); sys.exit(0)"])
+    assert help_result.exit_code == 0
+    assert len(help_result.output) == 0
+
+
+def test_error():
+    """Test that the error is reported."""
+    runner = CliRunner()
+    help_result = runner.invoke(cli.main, ["--", "python", "-c", "import sys; print('hello'); sys.exit(1)"])
+    assert help_result.exit_code == 1
+    assert re.search(r"cronshield detected failure or error output for the command", help_result.output) is not None
+    assert "hello" in help_result.output
